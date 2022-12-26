1182805393c71620b90fa0b05ae8223b63348518
Lionen89 <Lionen89@yandex.ru>
2022-12-26 22:04:24 +0700

    fix/added styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import List from './components/List';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import listTitels from './constansts';
 
 function App() {
-  const listTitels = ['Backlog', 'To Do', 'Review', 'Done'];
   return (
     <div className="main-page">
       <div className="main-page__container">
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,6 +4,8 @@ function List({ name }) {
   return (
     <div className="list">
       <h2 className="list__title">{name}</h2>
+      <p className="list__text">Task text</p>
+      <button className="list__button">Add Task</button>
     </div>
   );
 }
